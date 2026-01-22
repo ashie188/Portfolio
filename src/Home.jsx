b9a0959eca4d95a1d01760380d0cdf943c498084
ashie188 <2022bit028@sggs.ac.in>
2026-01-22 23:12:55 +0530

    Initial Commit

---
+++ src/Home.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <section className="hero">
+        <div class="header">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Contact</a>
+          </div>
+        <div className="overlay">
+          <div className="hero-content">
+            <div className="text-section">
+              <h1>Ashutosh Hamand</h1>
+              <p>I'm a Full-Stack Developer passionate about building scalable web applications and solving problems with code.</p>
+              <h2>Full-Stack Developer</h2>
+            </div>
+            <div className="image-section">
+              <img src="/profile.jpg" alt="Profile" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="techstackbackground">
+      <div className="techstack">
+        <h2>What i do?</h2>
+        <h1 class="expertise-title">
+  <span>A</span><span>r</span><span>e</span><span>a</span><span>s</span><span>&nbsp;</span>
+  <span>o</span><span>f</span><span>&nbsp;</span>
+  <span>E</span><span>x</span><span>p</span><span>e</span><span>r</span><span>t</span><span>i</span><span>s</span><span>e</span>
+        </h1>
+
+        <div className="techstackbuttons">
+          <p className="stack">DSA</p>
+          <p className="stack">Java</p>
+          <p className="stack">Python</p>
+          <p className="stack">Html</p>
+          <p className="stack">CSS</p>
+          <p className="stack">JavaScript</p>
+          <p className="stack">React</p>  
+          <p className="stack">Nodejs</p>
+          <p className="stack">Express</p>
+          <p className="stack">Ejs</p>
+          <p className="stack">SQL</p>
+          <p className="stack">Postgress</p>
+        </div>
+      </div>
+      </div>
+      <div className="projects">
+        <div className="innerprojects">
+          <div className="innerprojectimg">
+            <img src="./E-Commerce_ss.png" alt="ecommerce_img" width="320px" height="350px"/>
+          </div>
+          <div className="innerprojectcontent">
+            <p>about to write the e-commerce info</p>
+          </div>
+        </div>
+        <div className="innerprojects">
+          
+        </div>
+        <div className="innerprojects">
+          
+        </div>
+        <div className="innerprojects">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*<div>
+      <div className="relative w-64 h-64">
+        <img style={{marginLeft:150}} src="./main_background.png" alt="bg" className="absolute inset-0 w-full h-full object-cover animate-floating" height="650px" width="80%"/>
+        <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+        <div className="relative z-10 text-center text-white">
+          <img src="/profile.jpg" alt="profile" className="w-48 h-48 rounded-full mx-auto border-4 border-cyan-400 shadow-lg" />
+          <h1 className="mt-6 text-3xl font-bold">Welcome to Ashutosh Hamand's Portfolio</h1>
+          <p className="mt-2 text-lg text-gray-300">Full Stack Developer | Problem Solver | Tech Enthusiast</p>
+        </div>
+      </div>
+    </div>*/
---
+++ src/Project.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Project(){
+  return(
+    <div >
+      <div>
+
+      </div>
+    </div>
+  );
+}
